8c5c3a5f8c509296c8bb8bf7f2878c144d9ba4c2
Tzur1234 <113801007+Tzur1234@users.noreply.github.com>
2022-12-07 19:41:21 +0000

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")  
 def home():
-    return "Hello! this is the main page <h1>HELLO</h1>" 
+    return "Hello! <h1>HELLO</h1>" 
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
